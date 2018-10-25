651044e15bfeb077adfb7588970de385b14c4cb2
morrowwolf <darthbane97@gmail.com>
2018-10-24 19:52:36 -0600

    Fucking Travis dude

--- tools/travis/install_build_tools.sh
+++ tools/travis/install_build_tools.sh
@@ -5,7 +5,7 @@ source dependencies.sh
 
 if [ "$BUILD_TOOLS" = true ]; then
       rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $NODE_VERSION
-      npm install -g gulp-cli
+      npm install -g gulp-cli --ignore-scripts
       pip3 install --user PyYaml -q
       pip3 install --user beautifulsoup4 -q
 fi;
