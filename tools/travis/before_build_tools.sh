caf9c82ad908785cd1305962e7e91d75bef9a13d
morrowwolf <darthbane97@gmail.com>
2018-11-23 05:10:52 -0700

    Come on Travis we can talk about this

--- tools/travis/before_build_tools.sh
+++ tools/travis/before_build_tools.sh
@@ -2,6 +2,6 @@
 set -e
 
 if [ "$BUILD_TOOLS" = true ]; then
-    cd tgui && source ~/.nvm/nvm.sh && npm install && npm cache clean --force && cd ..
+    cd tgui && source ~/.nvm/nvm.sh && npm install && npm cache clean --force && DEL "package-lock.json" && cd ..
 fi;
 
